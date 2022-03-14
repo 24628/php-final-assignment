ef1b949ea7a340c43e2aa5bc0a38ff4f7fd9fa46
24628 <merlijn1busch@gmail.com>
2022-03-14 14:15:53 +0100

    Herku :D

--- app/public/install.php
+++ app/public/install.php
@@ -30,8 +30,8 @@ try {
     $connection->query(  "INSERT INTO users (id, name, password) VALUES (1, 'admin', 'password');");
 
     $connection->query(  "INSERT INTO posts (id, title, description) VALUES
-(2, 'Some title 2', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially '),
-(3, 'Second title', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ');");
+(2, 'Some title 2', 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially '),
+(3, 'Second title', 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially ');");
 } catch (PDOException $e) {
     echo "Query failed: " . $e->getMessage();
 }
